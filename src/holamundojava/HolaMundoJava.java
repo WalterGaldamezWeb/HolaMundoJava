db88698e44698c6bf68b19db3387e4686e9f4812
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2019-12-27 17:24:37 -0600

    Hola Mundo con Java

---
+++ src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,29 @@
+package holamundojava;
+
+ // @author Walter Galdamez
+
+public class HolaMundoJava {
+
+    public static void main(String[] args) {
+        /* Ejercicio 1 Hola Mundo con Java
+
+        --Nombre del paquete--
+        package HolaMundo;
+
+        --Nombre de la Clase--
+        public class HolaMundo{
+
+        --Metodo main para ejecutar una clase standard de java--
+
+            public static void main(String [] args){
+        --Metodo println para imprimir en pantalla--
+
+                System.out.println("Hola Mundo cmcon java");
+            }
+        }
+
+        */
+        System.out.println("Hola Mundo con Java");
+    }
+    
+}
